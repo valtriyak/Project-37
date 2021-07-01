aa7f4aeb874f90969f3c670571f76e03724edeb6
valtriyak <73747662+valtriyak@users.noreply.github.com>
2021-07-01 21:09:11 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,65 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    Question.hide();
+  
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      for(var plr in allPlayers){
+        if (plr === "player" + player.index)
+          fill("red")
+        else
+          fill("black");
+
+        
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+    //write code to change the background color here
+
+    //write code to show a heading for showing the result of Quiz
+
+    //call getContestantInfo( ) here
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
